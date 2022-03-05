dc54b7498e0c6ad82767711814a9963f9fa34513
Vo Minh Thu <thu@smart.coop>
2022-03-05 20:16:07 +0100

    Add helper scripts for development.

---
+++ scripts/run-example.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -i bash ../shell.nix
+
+# See ghci.sh for comments.
+
+ghc --interactive \
+  -iexample/executable/ \
+  -iexample/src/ \
+  -ilib/src/ \
+  -hide-all-packages -Wmissing-home-modules -no-user-package-db -package-db /home/thu/.cabal/store/ghc-8.6.5/package.db -package-db /home/thu/work/smart/prototype-hs/dist-newstyle/packagedb/ghc-8.6.5 -package-db /home/thu/work/smart/prototype-hs/dist-newstyle/build/x86_64-linux/ghc-8.6.5/prototype-hs-example-0.1.0.0/x/prototype-hs-example-exe/package.conf.inplace -package-id 'start-servant-0.1.0.0-2UOR3nYE030KpKThol2Hib (Prototype.ACL, Prototype.ACL.Types, Prototype.Data.Examples, Prototype.Grantee.Group, Prototype.Html, Prototype.Module, Prototype.Pages.Home, Prototype.Runtime, Prototype.Runtime.Errors, Prototype.Runtime.StmDatabase, Prototype.Runtime.Storage, Prototype.Server.Legacy, Prototype.Server.Legacy.Auth, Prototype.Server.New, Prototype.Server.New.Auth, Prototype.Server.New.Page, Prototype.Server.New.Page.Shared, Prototype.Server.New.Page.Shared.ViewMode, Prototype.Server.New.Page.UserPages, Prototype.Server.New.Page.UserPages.Todos, Prototype.Server.New.Page.UserPages.Todos.Item, Prototype.Server.New.StartPage, Prototype.Server.New.Todos, Prototype.Types, Prototype.Types.NonEmptyText, Prototype.Types.Secret)' -package-id async-2.2.2-6rOJAIn6doQ8bltUxcy0dW -package-id base-noprelude-4.12.0.0-LO8XhqS01WD4E3cSCpqQ2C -package-id containers-0.6.0.1 -package-id data-default-class-0.1.2.0-FeIQ5tLoVZBHMSgrT9zptQ -package-id http-types-0.12.3-F2xMhplkW5dAYXtwWx85Jd -package-id lens-4.17.1-OI9xiiJY6KIEwKnz7hWvu -package-id megaparsec-7.0.5-FFzGz4dCh1H9yigxOvQWbt -package-id optparse-applicative-0.14.3.0-KgUFYVq9ZZSIX6QVzTkb7A -package-id pretty-simple-2.2.0.1-3sR90XMyE51FkTGrLwHR9i -package-id protolude-0.2.3-7xU7UxDZe4gBg92V9qpGfW -package-id stm-2.5.0.0 -package-id text-1.2.3.1 -package readline \
+  -XHaskell2010 -XStrictData -XMultiParamTypeClasses -XDerivingStrategies -XDerivingVia -XDeriveGeneric -XRecordWildCards -XTypeSynonymInstances -XFlexibleInstances -XFlexibleContexts -XUndecidableInstances -XLambdaCase -XTypeApplications -XScopedTypeVariables -XGADTs -XOverloadedStrings -XPackageImports example/executable/ExampleMain.hs \
+  -e ":main --server-port 9000 --repl-prompt \"> \" --repl-history-on --repl-exit-cmd exit"
