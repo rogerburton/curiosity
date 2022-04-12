152a575ad2e611f50b8e1d09f0c4de3cc477644c
Vo Minh Thu <thu@smart.coop>
2022-04-12 16:11:21 +0200

    Update ghci.sh helper script.
    It is now slightly more complicated because we rely on the Prelude
    provided by design-hs. Have a look at the script for additional
    comments.

--- scripts/ghcid.sh
+++ scripts/ghcid.sh
@@ -1,8 +1,4 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -i bash ../shell.nix
 
-ghcid --command 'cabal repl prototype-hs-example-exe'
-
-# This starts faster but I guess that script will get obsolete quickly.
-#
-#   ghcid --command ./ghci.sh
+ghcid --command scripts/ghci.sh
