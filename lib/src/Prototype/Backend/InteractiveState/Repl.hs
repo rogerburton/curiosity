da746db46327fcc3c68bdf7d9b123ec90aea0c4a
Vo Minh Thu <thu@smart.coop>
2022-04-13 10:08:27 +0200

    Replace Readline by Haskeline.
    The main reason to do so was to solve:
    
    https://github.com/asheshambasta/prototype-hs/issues/6
    
    where it seemed that the output of the REPL was mixed up with the output
    of the server. Something similar was also happening without the server
    code but when using the REPL within GHCi.
    
    I no longer see the same behavior after the change.
    
    It's also interesting to note that Haskeline is BSD-3 while Readline is
    GPL (since it's a binding to GNU Readline) and has many more reverse
    dependencies on Hackage:
    
    https://packdeps.haskellers.com/reverse/haskeline
    
    Note that the previous loop code was using an argument to decide whether
    to continue or not (ReplContinue) but this didn't seem used.

--- lib/src/Prototype/Backend/InteractiveState/Repl.hs
+++ lib/src/Prototype/Backend/InteractiveState/Repl.hs
@@ -18,16 +18,19 @@ import qualified Prototype.Backend.InteractiveState.Class
 import qualified Prototype.Runtime.Errors      as Errs
 import           Prototype.Types.NonEmptyText
 import           PrototypeHsLibPrelude
-import qualified System.Console.Readline       as RL
+import qualified System.Console.Haskeline      as HL
 
 -- | Newtype over exit commands for type-safety.
 newtype ExitCmd = ExitCmd NonEmptyText
                 deriving (Eq, Show, IsString) via NonEmptyText
 
 data ReplConf = ReplConf
-  { _replPrompt       :: Text -- ^ A prompt for the REPL, must not include trailing spaces. 
-  , _replHistory      :: Bool  -- ^ Do we want to have repl-history? 
-  , _replReplExitCmds :: [ExitCmd] -- ^ The list of exit commands we want to exit with
+  { _replPrompt       :: Text
+    -- ^ A prompt for the REPL, must not include trailing spaces.
+  , _replHistory      :: Bool
+    -- ^ Do we want to have repl-history? TODO: Currently unused.
+  , _replReplExitCmds :: [ExitCmd]
+    -- ^ The list of exit commands we want to exit with
   }
   deriving Show
 
@@ -35,71 +38,54 @@ instance Default ReplConf where
   def = ReplConf "% " True ["exit"]
 
 data ReplLoopResult where
-  -- | Some fatal exception that caused the repl exit; more like a catch-all. 
+  -- | Some fatal exception that caused the repl exit; more like a catch-all.
   ReplExitOnGeneralException ::Exception e => e -> ReplLoopResult
   -- | The user explicitly indicated they'd like to exit the repl.
   ReplExitOnUserCmd ::Text -> ReplLoopResult
-  -- | Continue the repl loop.
-  ReplContinue ::ReplLoopResult
 
 deriving instance Show ReplLoopResult
 
--- | Start up the REPL.
 startReadEvalPrintLoop
   :: forall m
    . MonadIO m
-  => ReplConf -- ^ The configuration to run with.
-  -> (IS.DispInput 'IS.Repl -> m (IS.DispOutput 'IS.Repl)) -- ^ Function mapping the repl input to output.
-  -> (forall a . m a -> IO (Either Errs.RuntimeErr a)) -- ^ Instructions on how to run some @m@ into @IO@
-  -> m ReplLoopResult -- ^ The reason for user-exit. 
+  => ReplConf
+     -- ^ The configuration to run with.
+  -> (IS.DispInput 'IS.Repl -> m (IS.DispOutput 'IS.Repl))
+     -- ^ Function mapping the repl input to output.
+  -> (forall a . m a -> IO (Either Errs.RuntimeErr a))
+     -- ^ Instructions on how to run some @m@ into @IO@
+  -> m ReplLoopResult
+     -- ^ The reason for user-exit.
 startReadEvalPrintLoop ReplConf {..} processInput runMInIO =
-  liftIO . mapSomeEx $ do
-    RL.initialize
-    RL.addHistory "viz user UserLogin \"1\" \"pass\""
-    RL.addHistory "mod user UserCreate \"1\" \"alice\" \"pass\""
-    loopRepl ReplContinue
+  liftIO . mapSomeEx $ HL.runInputT HL.defaultSettings loop
  where
-  loopRepl ReplContinue = RL.readline prompt >>= \case
-    Nothing -> output' "" $> ReplExitOnUserCmd "eof"
-    -- TODO Probably processInput below
-    -- (within parseAnyStateInput) should have other possible results (beside mod and
-    -- viz): comments and blanks (no-op), instead of this special empty case.
-    Just "" -> loopRepl ReplContinue
-    Just cmdString
-      | isReplExit
-      -> pure $ ReplExitOnUserCmd cmd
-      | otherwise
-      -> let replInput = IS.ReplInputStrict cmd
-         in
-          -- Add the input to the history, process it, and write the output to the output stream.
-           do
-
-             when _replHistory $ RL.addHistory cmdString
-
-             -- Process input.
-             output <- runMInIO $ processInput replInput
-
-             case output of
-               Right (IS.ReplOutputStrict txt ) -> output' txt
-               Right (IS.ReplOutputLazy   txtL) -> output' $ TL.toStrict txtL
-               Right (IS.ReplRuntimeErr   err ) -> output' $ Errs.displayErr err
-               Left rtErr ->
-                 output' $ "Unhandled runtime error: " <> Errs.displayErr rtErr
-
-             loopRepl ReplContinue
-     where
-      isReplExit = case nonEmptyText cmd of
-        Nothing    -> False
-        Just neCmd -> ExitCmd neCmd `elem` _replReplExitCmds
-      cmd = T.pack $ cmdString
-
-  loopRepl exit@ReplExitOnUserCmd{}          = pure exit
-  loopRepl exit@ReplExitOnGeneralException{} = pure exit
-
-  output' txt = RL.getOutStream >>= (`T.IO.hPutStrLn` txt)
-
   mapSomeEx op =
     try @SomeException op <&> either ReplExitOnGeneralException identity
 
-  prompt = T.unpack _replPrompt
+  loop = HL.getInputLine prompt >>= \case
+    Nothing                       -> output' "" $> ReplExitOnUserCmd "eof"
+    -- TODO Probably processInput below (within parseAnyStateInput) should have
+    -- other possible results (beside mod and viz): comments and blanks
+    -- (no-op), instead of this special empty case.
+    Just ""                       -> loop
+    Just input | isReplExit input -> pure . ReplExitOnUserCmd $ T.pack input
+    Just input                    -> do
+      let input' = IS.ReplInputStrict $ T.pack input
+      output <- liftIO . runMInIO $ processInput input'
 
+      case output of
+        Right (IS.ReplOutputStrict txt ) -> output' txt
+        Right (IS.ReplOutputLazy   txtL) -> output' $ TL.toStrict txtL
+        Right (IS.ReplRuntimeErr   err ) -> output' $ Errs.displayErr err
+        Left rtErr ->
+          output' $ "Unhandled runtime error: " <> Errs.displayErr rtErr
+
+      loop
+
+  prompt  = T.unpack _replPrompt
+
+  output' = HL.outputStrLn . T.unpack
+
+  isReplExit txt = case nonEmptyText $ T.pack txt of
+    Nothing    -> False
+    Just neCmd -> ExitCmd neCmd `elem` _replReplExitCmds
