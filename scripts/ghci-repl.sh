c922527762082a80f73268c754989b5e7ad87ad5
Vo Minh Thu <thu@smart.coop>
2022-05-11 07:56:59 +0200

    Add a cty-repl program.
    This runs only the REPL, unlike prototype-hs-exe that also runs
    the HTTP server.

---
+++ scripts/ghci-repl.sh
@@ -0,0 +1,34 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -i bash ../shell.nix
+
+# This is a bit convoluted because the Prelude is from design-hs:
+#   - -XNoImplicitPrelude is set here on the command-line
+#   - The ghci.conf file will load design-hs's Prelude
+#   - Then set again -XImplicitPrelude
+#   - Then load what we want: cty-repl.hs
+
+ghc --interactive \
+  -i../design-hs/lib/src/ \
+  -iexe/executable/ \
+  -iexe/src/ \
+  -ilib/src/ \
+  -hide-package base \
+  -XNoImplicitPrelude \
+  -XHaskell2010 \
+  -XStrictData \
+  -XMultiParamTypeClasses \
+  -XDerivingStrategies \
+  -XDerivingVia \
+  -XDeriveGeneric \
+  -XRecordWildCards \
+  -XTypeSynonymInstances \
+  -XFlexibleInstances \
+  -XFlexibleContexts \
+  -XUndecidableInstances \
+  -XLambdaCase \
+  -XTypeApplications \
+  -XScopedTypeVariables \
+  -XGADTs \
+  -XOverloadedStrings \
+  -XPackageImports \
+  -ghci-script scripts/ghci-repl.conf
