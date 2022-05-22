ec93d245612039d3eeb9ea08cb00332182f62585
Ashesh Ambasta <ashesh.ambasta@gmail.com>
2022-05-22 11:31:34 +0200

    Remove executable (exe)
    @noteed, the builds don't really work on my system.
    
    See:
    ```
    [    1.381387] EXT4-fs (vda1): mounted filesystem with ordered data mode. Opts:
    [    1.412743] random: fast init done
    [    5.209047] random: crng init done
    error writing file: No space left on device
    error processing entry /build/root/nix/store/sal8ji25wf0480mh65msvl14pim09xbw-design-hs-lib-0.1.0.0/lib/ghc-8.6.5/x86_64-linux-ghc-8.6.5/design-hs-lib-0.1.0.0-HoLSqH62qrkBb2Z90KS1ND/libHSdesign-hs-lib-0.1.0.0-HoLSqH62qrkBb2Z90KS1ND_p.a, aborting
    error processing entry /build/root/nix/store/sal8ji25wf0480mh65msvl14pim09xbw-design-hs-lib-0.1.0.0/lib/ghc-8.6.5/x86_64-linux-ghc-8.6.5/design-hs-lib-0.1.0.0-HoLSqH62qrkBb2Z90KS1ND, aborting
    error processing entry /build/root/nix/store/sal8ji25wf0480mh65msvl14pim09xbw-design-hs-lib-0.1.0.0/lib/ghc-8.6.5/x86_64-linux-ghc-8.6.5, aborting
    error processing entry /build/root/nix/store/sal8ji25wf0480mh65msvl14pim09xbw-design-hs-lib-0.1.0.0/lib/ghc-8.6.5, aborting
    error processing entry /build/root/nix/store/sal8ji25wf0480mh65msvl14pim09xbw-design-hs-lib-0.1.0.0/lib, aborting
    error processing entry /build/root/nix/store/sal8ji25wf0480mh65msvl14pim09xbw-design-hs-lib-0.1.0.0, aborting
    error processing entry /build/root/nix/store, aborting
    error processing entry /build/root/nix, aborting
    [   35.626990] reboot: Restarting system
    builder for '/nix/store/d1r2rwbp7wf0nb9xha6p67azi8138d4l-digital-ocean-image.drv' failed with exit code 228
    error: build of '/nix/store/d1r2rwbp7wf0nb9xha6p67azi8138d4l-digital-ocean-image.drv' failed
    ```

--- nix/contents.nix
+++ nix/contents.nix
@@ -10,8 +10,7 @@ in {
   # Lists all packages made available through this nix project.
   # The format is `{ <pkgName> : <pkgDir> }` (we refer to this as pInfo)
   # The used directory should be the path of the directory relative to the root of the project.
-  pkgList = { prototype-hs-exe = ../exe;
-              prototype-hs-lib = ../lib;
+  pkgList = { prototype-hs-lib = ../lib;
               prototype-hs-example = ../example;
             };
   # Get an attribute from a string path from a larger attrSet
