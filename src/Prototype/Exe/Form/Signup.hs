cea64462a2170738665c45f20e0b33fcdd39c4fb
Vo Minh Thu <thu@hypered.io>
2022-07-04 10:47:28 +0200

    Add new signup page.
    In addition of the normal route, an additional copy of the form and a
    echo handler are added. In the future, I think we would also add a
    handler to run the validation code but not the complete server logic.

---
+++ src/Prototype/Exe/Form/Signup.hs
@@ -0,0 +1,160 @@
+{-# LANGUAGE DeriveAnyClass #-}
+module Prototype.Exe.Form.Signup
+  ( Input
+  , Page(..)
+  , ResultPage(..)
+  ) where
+
+import           Smart.Html.Dsl                 ( HtmlCanvas )
+import qualified Smart.Html.Dsl                as Dsl
+import qualified Smart.Html.Render             as Render
+import           Smart.Html.Shared.Html.Icons
+import           Text.Blaze                     ( customAttribute )
+import qualified Text.Blaze.Html5              as H
+import           Text.Blaze.Html5               ( (!) )
+import qualified Text.Blaze.Html5.Attributes   as A
+import           Web.FormUrlEncoded             ( FromForm(..)
+                                                , parseUnique
+                                                )
+
+
+--------------------------------------------------------------------------------
+data Input = Input
+  { username     :: Text
+  , emailAddress :: Text
+  , password     :: Text
+  , iUnderstand  :: Text -- TODO Bool
+  }
+  deriving (Eq, Show)
+
+instance FromForm Input where
+  fromForm f =
+    Input
+      <$> parseUnique "username"     f
+      <*> parseUnique "email"        f
+      <*> parseUnique "password"     f
+      <*> parseUnique "i-understand" f
+
+
+--------------------------------------------------------------------------------
+data Page = Page
+  { signupSubmitURL :: H.AttributeValue
+  }
+
+instance H.ToMarkup Page where
+  toMarkup = Render.renderCanvas . signupPage
+
+signupPage :: Page -> HtmlCanvas
+signupPage Page {..} = Dsl.SingletonCanvas $ do
+  H.header
+    ! A.class_ "o-container-vertical"
+    $ H.div
+    ! A.class_ "o-container"
+    $ H.div
+    ! A.class_ "c-brand c-brand--xsmall"
+    $ H.a
+    ! A.href "/"
+    $ H.img
+    ! A.src "https://design.smart.coop/images/logo.svg"
+    ! A.alt "Smart"
+  H.main
+    ! A.class_ "o-container-vertical"
+    $ H.div
+    ! A.class_ "o-container o-container--small"
+    $ do
+        H.div
+          ! A.class_ "c-panel"
+          $ H.div
+          ! A.class_ "c-panel__body"
+          $ H.form
+          $ do
+              H.h3 ! A.class_ "c-h2" $ "Create your account"
+              H.div
+                ! A.class_ "o-form-group-layout o-form-group-layout--standard"
+                $ do
+                    H.div ! A.class_ "o-form-group" $ do
+                      H.label
+                        ! A.class_ "o-form-group__label"
+                        ! A.for "username"
+                        $ "Username"
+                      H.div
+                        ! A.class_ "o-form-group__controls"
+                        $ H.input
+                        ! A.class_ "c-input"
+                        ! A.id "username"
+                        ! A.name "username"
+                        ! A.type_ "text"
+                    H.div ! A.class_ "o-form-group" $ do
+                      H.label
+                        ! A.class_ "o-form-group__label"
+                        ! A.for "email"
+                        $ "Email address"
+                      H.div
+                        ! A.class_ "o-form-group__controls"
+                        $ H.input
+                        ! A.class_ "c-input"
+                        ! A.id "email"
+                        ! A.name "email"
+                        ! A.type_ "email"
+                    H.div ! A.class_ "o-form-group" $ do
+                      H.label
+                        ! A.class_ "o-form-group__label"
+                        ! A.for "password"
+                        $ "Password"
+                      H.div
+                        ! A.class_ "o-form-group__controls"
+                        $ H.div
+                        ! A.class_ "c-input-with-icon"
+                        $ do
+                            H.input
+                              ! A.class_ "c-input"
+                              ! A.id "password"
+                              ! A.name "password"
+                              ! A.type_ "password"
+                            H.button
+                              ! A.class_ "c-input-with-icon__toggle"
+                              ! customAttribute "data-password-toggle" "1"
+                              $ do
+                                  H.div
+                                    ! A.class_ "o-svg-icon o-svg-icon-eye"
+                                    $ H.toMarkup svgIconEye
+                                  H.div
+                                    ! A.class_ "o-svg-icon o-svg-icon-eye-off"
+                                    $ H.toMarkup svgIconEyeOff
+                    H.div ! A.class_ "o-form-group" $ do
+                      H.div ! A.class_ "c-checkbox" $ do
+                        H.label $ do
+                          H.input
+                            ! A.id "i-understand"
+                            ! A.name "i-understand"
+                            ! A.type_ "checkbox"
+                          H.div $ do
+                            "I understand that this site is up for "
+                            "demonstration purpose only, and that data are "
+                            "regularly erased."
+                            H.a ! A.href "#" $ "Read more."
+                    H.div
+                      ! A.class_ "o-form-group"
+                      $ H.button
+                      ! A.class_ "c-button c-button--primary c-button--block"
+                      ! A.formaction signupSubmitURL
+                      ! A.formmethod "POST"
+                      $ H.span
+                      ! A.class_ "c-button__content"
+                      $ "Sign up"
+                    H.div ! A.class_ "o-form-group u-ta-center" $ ""
+        H.div ! A.class_ "c-content u-text-center u-spacer-top-l" $ do
+          H.a ! A.class_ "u-text-muted" ! A.href "#" $ "Log in"
+
+
+--------------------------------------------------------------------------------
+data ResultPage = Success Text
+                | Failure Text
+
+instance H.ToMarkup ResultPage where
+  toMarkup = \case
+    Success msg -> withText msg
+    Failure msg -> withText msg
+   where
+    withText msg =
+      Render.renderCanvas $ Dsl.SingletonCanvas $ H.code $ H.toMarkup msg
--- src/Prototype/Exe/Server.hs
+++ src/Prototype/Exe/Server.hs
@@ -23,6 +23,7 @@ import qualified Network.HTTP.Types            as HTTP
 import qualified Network.Wai                   as Wai
 import qualified Network.Wai.Handler.Warp      as Warp
 import           Prototype.Exe.Data.User        ( )
+import qualified Prototype.Exe.Form.Signup     as Signup
 import qualified Prototype.Exe.Runtime         as Rt
 import qualified Prototype.Exe.Server.Private  as Priv
 import qualified Prototype.Exe.Server.Public   as Pub
@@ -39,13 +40,28 @@ type ServerSettings = '[Srv.CookieSettings , Srv.JWTSettings]
 
 -- brittany-disable-next-binding 
 type Exe = Get '[B.HTML] Pages.LandingPage
+             :<|> "forms" :> "signup" :> Get '[B.HTML] Signup.Page
+             :<|> "echo" :> "signup"
+                  :> ReqBody '[FormUrlEncoded] Signup.Input
+                  :> Post '[B.HTML] Signup.ResultPage
+             :<|> "signup" :> Get '[B.HTML] Signup.Page
+             :<|> "a" :> "signup"
+                  :> ReqBody '[FormUrlEncoded] Signup.Input
+                  :> Post '[B.HTML] Signup.ResultPage
              :<|> "public" :> Pub.Public
              :<|> "private" :> Priv.Private
              :<|> Raw -- catchall for custom 404
 
 exampleT :: forall m . Pub.PublicServerC m => ServerT Exe m
 exampleT =
-  showLandingPage :<|> Pub.publicT :<|> Priv.privateT :<|> pure custom404
+  showLandingPage
+    :<|> documentSignupPage
+    :<|> echoSignup
+    :<|> showSignupPage
+    :<|> handleSignup
+    :<|> Pub.publicT
+    :<|> Priv.privateT
+    :<|> pure custom404
 
 -- | Run as a Wai Application
 exampleApplication
@@ -79,6 +95,18 @@ runExeServer runtime@Rt.Runtime {..} = liftIO $ Warp.run port waiApp
 
 showLandingPage :: Pub.PublicServerC m => m Pages.LandingPage
 showLandingPage = pure Pages.LandingPage
+
+showSignupPage :: Pub.PublicServerC m => m Signup.Page
+showSignupPage = pure $ Signup.Page "/a/signup"
+
+documentSignupPage :: Pub.PublicServerC m => m Signup.Page
+documentSignupPage = pure $ Signup.Page "/echo/signup"
+
+handleSignup :: Pub.PublicServerC m => Signup.Input -> m Signup.ResultPage
+handleSignup _ = pure $ Signup.Failure "TODO handleSignup"
+
+echoSignup :: Pub.PublicServerC m => Signup.Input -> m Signup.ResultPage
+echoSignup input = pure $ Signup.Success $ show input
 
 custom404 :: Application
 custom404 _request sendResponse = sendResponse $ Wai.responseLBS
