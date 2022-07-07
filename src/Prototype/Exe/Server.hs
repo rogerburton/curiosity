8b2c94896f1ea6fe06fdc5a4cbff4b1c0dd5820b
Vo Minh Thu <thu@hypered.io>
2022-07-07 17:48:57 +0200

    Allow nicer URLs for the documentation files.
    We have a special lookup function that appends ".html" to URLs before
    trying to load a file.

--- src/Prototype/Exe/Server.hs
+++ src/Prototype/Exe/Server.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE DataKinds
@@ -23,6 +24,10 @@ import qualified Commence.Multilogging         as ML
 import qualified Commence.Runtime.Errors       as Errs
 import qualified Commence.Runtime.Storage      as S
 import           Control.Lens
+import qualified Data.ByteString.Lazy          as BL
+                                                ( hGetContents )
+import           Data.List                      ( init, last )
+import qualified Data.Text                     as T
 import qualified Network.HTTP.Types            as HTTP
 import qualified Network.Wai                   as Wai
 import qualified Network.Wai.Handler.Warp      as Warp
@@ -44,11 +49,36 @@ import qualified Servant.HTML.Blaze            as B
 import qualified Servant.Server                as Server
 import           Smart.Server.Page              ( PageEither )
 import qualified Smart.Server.Page             as SS.P
+import           System.FilePath                ( (</>) )
+import           System.PosixCompat.Files       ( fileSize
+                                                , getFileStatus
+                                                , isRegularFile
+                                                , modificationTime
+                                                )
 import qualified Text.Blaze.Html5              as H
 import           Text.Blaze.Renderer.Utf8       ( renderMarkup )
 import           WaiAppStatic.Storage.Filesystem
-                                                ( defaultWebAppSettings )
-import           WaiAppStatic.Types             ( ss404Handler )
+                                                ( ETagLookup
+                                                , defaultWebAppSettings
+                                                )
+import           WaiAppStatic.Types             ( File(..)
+                                                , LookupResult(..)
+                                                , Piece(..)
+                                                , Pieces
+                                                , ss404Handler
+                                                , ssLookupFile
+                                                , toPiece
+                                                )
+
+import           System.IO                      ( IOMode(..)
+                                                , withBinaryFile
+                                                )
+
+import           Data.ByteArray.Encoding
+import           Crypto.Hash                    ( Digest
+                                                , MD5
+                                                , hashlazy
+                                                )
 
 
 --------------------------------------------------------------------------------
@@ -240,11 +270,98 @@ handleLogin User.Credentials {..} =
 -- 404 fallback.
 serveDocumentation = serveDirectoryWith settings
  where
-  settings =
-    (defaultWebAppSettings "./_site/") { ss404Handler = Just custom404 }
+  settings = (defaultWebAppSettings root)
+    { ss404Handler = Just custom404
+    , ssLookupFile = webAppLookup hashFileIfExists root
+    }
+  root = "./_site/"
 
 custom404 :: Application
 custom404 _request sendResponse = sendResponse $ Wai.responseLBS
   HTTP.status404
   [("Content-Type", "text/html; charset=UTF-8")]
   (renderMarkup $ H.toMarkup Pages.NotFoundPage)
+
+
+--------------------------------------------------------------------------------
+-- Most of this is taken from WaiAppStatic.Storage.Filesystem.
+-- The goal is to allow to re-define the above `ssLookupFile` in
+-- `defaultWebAppSettings`: instead of looking up files as-is, we make it so
+-- that given a URL such as /some/path, we act as if it was /some/path.html,
+-- thus making nicer URLs (where the .html is not necessary).
+-- TODO Move this to hypered/commence.
+-- TODO Don't rewrite paths when there is an extension, otherwise it won't work
+-- for e.g. images.
+-- Note: we copy more code than necessary because it is not in the export list
+-- of the original module.
+
+-- | Construct a new path from a root and some @Pieces@.
+pathFromPieces :: FilePath -> Pieces -> FilePath
+pathFromPieces = foldl' (\fp p -> fp </> T.unpack (fromPiece p))
+
+-- | Construct a @Piece@ without input validation.
+-- The original can use a non-exported `Piece` constructor. Here we use the one
+-- that does some validation, but in an unsafe way.
+unsafeToPiece :: Text -> Piece
+unsafeToPiece t = let Just p = toPiece t in p
+
+-- | More efficient than @fileSystemLookup@ as it only concerns itself with
+-- finding files, not folders.
+-- This is the main change compared to WaiAppStatic.Storage.Filesystem:
+-- If the last piece is empty, this lookup index.html.
+-- Otherwise, this appends ".html" before trying to lookup that.
+-- This means that if you want `documentation/` to be a directory, you can
+-- decide if you want to support `documentation/`, by putting an `index.html`
+-- within, or support `documentation`, by making a `documentation.html` file
+-- instead.
+webAppLookup :: ETagLookup -> FilePath -> Pieces -> IO LookupResult
+webAppLookup hashFunc prefix pieces = fileHelperLR hashFunc fp lastPiece
+ where
+  fp = pathFromPieces prefix pieces'
+  pieces' = initPieces ++ [lastPiece]
+  (initPieces, lastPiece) | null pieces = ([], unsafeToPiece "index.html")
+                          | Just (last pieces) == toPiece "" = (init pieces, unsafeToPiece "index.html")
+                          | otherwise   = (init pieces, unsafeToPiece $ fromPiece (last pieces) <> ".html")
+
+-- | Convenience wrapper for @fileHelper@.
+fileHelperLR
+  :: ETagLookup
+  -> FilePath -- ^ file location
+  -> Piece -- ^ file name
+  -> IO LookupResult
+fileHelperLR a b c = fmap (maybe LRNotFound LRFile) $ fileHelper a b c
+
+-- | Attempt to load up a @File@ from the given path.
+-- This is taken and modified from WaiAppStatic.Storage.Filesystem.
+fileHelper
+  :: ETagLookup
+  -> FilePath -- ^ file location
+  -> Piece -- ^ file name
+  -> IO (Maybe File)
+fileHelper hashFunc fp name = do
+  efs <- try $ getFileStatus fp
+  case efs of
+    Left (_ :: SomeException)   -> return Nothing
+    Right fs | isRegularFile fs -> return $ Just File
+      { fileGetSize     = fromIntegral $ fileSize fs
+      , fileToResponse  = \s h -> Wai.responseFile s h fp Nothing
+      , fileName        = name
+      , fileGetHash     = hashFunc fp
+      , fileGetModified = Just $ modificationTime fs
+      }
+    Right _ -> return Nothing
+
+hashFileIfExists :: ETagLookup
+hashFileIfExists fp = do
+  res <- try $ hashFile fp
+  return $ case res of
+    Left  (_ :: SomeException) -> Nothing
+    Right x                    -> Just x
+
+-- | MD5 hash and base64-encode the file contents. Does not check if the file
+-- exists.
+hashFile :: FilePath -> IO ByteString
+hashFile fp = withBinaryFile fp ReadMode $ \h -> do
+  f <- BL.hGetContents h
+  let !hash = hashlazy f :: Digest MD5
+  return $ convertToBase Base64 hash
