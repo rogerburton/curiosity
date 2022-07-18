517e6079f6595a81733c803d946d319b0c043690
Vo Minh Thu <thu@hypered.io>
2022-07-18 16:11:19 +0200

    Create WaiAppStatic.Storage.Filesystem.Extended.
    This contains the code copied (and sligthly modified) from
    WaiAppStatic.Storage.Filesystem.
    
    This should be moved to Commence, or better, be upstreamed.

--- src/Prototype/Server.hs
+++ src/Prototype/Server.hs
@@ -25,12 +25,6 @@ import qualified Commence.Multilogging         as ML
 import qualified Commence.Runtime.Errors       as Errs
 import qualified Commence.Runtime.Storage      as S
 import           Control.Lens
-import qualified Data.ByteString.Lazy          as BL
-                                                ( hGetContents )
-import           Data.List                      ( init
-                                                , last
-                                                )
-import qualified Data.Text                     as T
 import qualified Network.HTTP.Types            as HTTP
 import qualified Network.Wai                   as Wai
 import qualified Network.Wai.Handler.Warp      as Warp
@@ -54,36 +48,15 @@ import qualified Servant.HTML.Blaze            as B
 import qualified Servant.Server                as Server
 import           Smart.Server.Page              ( PageEither )
 import qualified Smart.Server.Page             as SS.P
-import           System.FilePath                ( (</>) )
-import           System.PosixCompat.Files       ( fileSize
-                                                , getFileStatus
-                                                , isRegularFile
-                                                , modificationTime
-                                                )
 import qualified Text.Blaze.Html5              as H
 import           Text.Blaze.Renderer.Utf8       ( renderMarkup )
 import           WaiAppStatic.Storage.Filesystem
-                                                ( ETagLookup
-                                                , defaultWebAppSettings
+                                                ( defaultWebAppSettings
                                                 )
-import           WaiAppStatic.Types             ( File(..)
-                                                , LookupResult(..)
-                                                , Piece(..)
-                                                , Pieces
-                                                , ss404Handler
+import WaiAppStatic.Storage.Filesystem.Extended ( hashFileIfExists, webAppLookup )
+import           WaiAppStatic.Types             ( ss404Handler
                                                 , ssLookupFile
-                                                , toPiece
                                                 )
-
-import           System.IO                      ( IOMode(..)
-                                                , withBinaryFile
-                                                )
-
-import           Crypto.Hash                    ( Digest
-                                                , MD5
-                                                , hashlazy
-                                                )
-import           Data.ByteArray.Encoding
 
 
 --------------------------------------------------------------------------------
@@ -368,94 +341,3 @@ custom404 _request sendResponse = sendResponse $ Wai.responseLBS
   HTTP.status404
   [("Content-Type", "text/html; charset=UTF-8")]
   (renderMarkup $ H.toMarkup Pages.NotFoundPage)
-
-
---------------------------------------------------------------------------------
--- Most of this is taken from WaiAppStatic.Storage.Filesystem.
--- The goal is to allow to re-define the above `ssLookupFile` in
--- `defaultWebAppSettings`: instead of looking up files as-is, we make it so
--- that given a URL such as /some/path, we act as if it was /some/path.html,
--- thus making nicer URLs (where the .html is not necessary).
--- TODO Move this to hypered/commence.
--- TODO Don't rewrite paths when there is an extension, otherwise it won't work
--- for e.g. images.
--- Note: we copy more code than necessary because it is not in the export list
--- of the original module.
-
--- | Construct a new path from a root and some @Pieces@.
-pathFromPieces :: FilePath -> Pieces -> FilePath
-pathFromPieces = foldl' (\fp p -> fp </> T.unpack (fromPiece p))
-
--- | Construct a @Piece@ without input validation.
--- The original can use a non-exported `Piece` constructor. Here we use the one
--- that does some validation, but in an unsafe way.
-unsafeToPiece :: Text -> Piece
-unsafeToPiece t = let Just p = toPiece t in p
-
--- | More efficient than @fileSystemLookup@ as it only concerns itself with
--- finding files, not folders.
--- This is the main change compared to WaiAppStatic.Storage.Filesystem:
--- If the last piece is empty, this lookup index.html.
--- Otherwise, this appends ".html" before trying to lookup that.
--- This means that if you want `documentation/` to be a directory, you can
--- decide if you want to support `documentation/`, by putting an `index.html`
--- within, or support `documentation`, by making a `documentation.html` file
--- instead.
-webAppLookup :: ETagLookup -> FilePath -> Pieces -> IO LookupResult
-webAppLookup hashFunc prefix pieces = fileHelperLR hashFunc fp lastPiece
- where
-  fp      = pathFromPieces prefix pieces'
-  pieces' = initPieces ++ [lastPiece]
-  (initPieces, lastPiece)
-    | null pieces
-    = ([], unsafeToPiece "index.html")
-    | Just (last pieces) == toPiece ""
-    = (init pieces, unsafeToPiece "index.html")
-    | otherwise
-    = let lastP = case fromPiece (last pieces) of
-            s | T.isSuffixOf ".txt" s -> last pieces
-            s                         -> unsafeToPiece $ s <> ".html"
-      in  (init pieces, lastP)
-
--- | Convenience wrapper for @fileHelper@.
-fileHelperLR
-  :: ETagLookup
-  -> FilePath -- ^ file location
-  -> Piece -- ^ file name
-  -> IO LookupResult
-fileHelperLR a b c = fmap (maybe LRNotFound LRFile) $ fileHelper a b c
-
--- | Attempt to load up a @File@ from the given path.
--- This is taken and modified from WaiAppStatic.Storage.Filesystem.
-fileHelper
-  :: ETagLookup
-  -> FilePath -- ^ file location
-  -> Piece -- ^ file name
-  -> IO (Maybe File)
-fileHelper hashFunc fp name = do
-  efs <- try $ getFileStatus fp
-  case efs of
-    Left (_ :: SomeException)   -> return Nothing
-    Right fs | isRegularFile fs -> return $ Just File
-      { fileGetSize     = fromIntegral $ fileSize fs
-      , fileToResponse  = \s h -> Wai.responseFile s h fp Nothing
-      , fileName        = name
-      , fileGetHash     = hashFunc fp
-      , fileGetModified = Just $ modificationTime fs
-      }
-    Right _ -> return Nothing
-
-hashFileIfExists :: ETagLookup
-hashFileIfExists fp = do
-  res <- try $ hashFile fp
-  return $ case res of
-    Left  (_ :: SomeException) -> Nothing
-    Right x                    -> Just x
-
--- | MD5 hash and base64-encode the file contents. Does not check if the file
--- exists.
-hashFile :: FilePath -> IO ByteString
-hashFile fp = withBinaryFile fp ReadMode $ \h -> do
-  f <- BL.hGetContents h
-  let !hash = hashlazy f :: Digest MD5
-  return $ convertToBase Base64 hash
---
+++ src/WaiAppStatic/Storage/Filesystem/Extended.hs
@@ -0,0 +1,139 @@
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE DataKinds
+           , TypeOperators
+#-} -- Language extensions needed for servant.
+{- |
+Module: Prototype.Server
+Description: Server root module, split up into public and private sub-modules.
+
+-}
+module WaiAppStatic.Storage.Filesystem.Extended
+  ( hashFileIfExists
+  , webAppLookup
+  ) where
+
+import qualified Data.ByteString.Lazy          as BL
+                                                ( hGetContents )
+import           Data.List                      ( init
+                                                , last
+                                                )
+import qualified Data.Text                     as T
+import qualified Network.Wai                   as Wai
+import           System.FilePath                ( (</>) )
+import           System.PosixCompat.Files       ( fileSize
+                                                , getFileStatus
+                                                , isRegularFile
+                                                , modificationTime
+                                                )
+import           WaiAppStatic.Storage.Filesystem
+                                                ( ETagLookup
+                                                )
+import           WaiAppStatic.Types             ( File(..)
+                                                , LookupResult(..)
+                                                , Piece(..)
+                                                , Pieces
+                                                , toPiece
+                                                )
+
+import           System.IO                      ( IOMode(..)
+                                                , withBinaryFile
+                                                )
+
+import           Crypto.Hash                    ( Digest
+                                                , MD5
+                                                , hashlazy
+                                                )
+import           Data.ByteArray.Encoding
+
+
+--------------------------------------------------------------------------------
+-- Most of this is taken from WaiAppStatic.Storage.Filesystem.
+-- The goal is to allow to re-define the above `ssLookupFile` in
+-- `defaultWebAppSettings`: instead of looking up files as-is, we make it so
+-- that given a URL such as /some/path, we act as if it was /some/path.html,
+-- thus making nicer URLs (where the .html is not necessary).
+-- TODO Move this to hypered/commence.
+-- TODO Don't rewrite paths when there is an extension, otherwise it won't work
+-- for e.g. images.
+-- Note: we copy more code than necessary because it is not in the export list
+-- of the original module.
+
+-- | Construct a new path from a root and some @Pieces@.
+pathFromPieces :: FilePath -> Pieces -> FilePath
+pathFromPieces = foldl' (\fp p -> fp </> T.unpack (fromPiece p))
+
+-- | Construct a @Piece@ without input validation.
+-- The original can use a non-exported `Piece` constructor. Here we use the one
+-- that does some validation, but in an unsafe way.
+unsafeToPiece :: Text -> Piece
+unsafeToPiece t = let Just p = toPiece t in p
+
+-- | More efficient than @fileSystemLookup@ as it only concerns itself with
+-- finding files, not folders.
+-- This is the main change compared to WaiAppStatic.Storage.Filesystem:
+-- If the last piece is empty, this lookup index.html.
+-- Otherwise, this appends ".html" before trying to lookup that.
+-- This means that if you want `documentation/` to be a directory, you can
+-- decide if you want to support `documentation/`, by putting an `index.html`
+-- within, or support `documentation`, by making a `documentation.html` file
+-- instead.
+webAppLookup :: ETagLookup -> FilePath -> Pieces -> IO LookupResult
+webAppLookup hashFunc prefix pieces = fileHelperLR hashFunc fp lastPiece
+ where
+  fp      = pathFromPieces prefix pieces'
+  pieces' = initPieces ++ [lastPiece]
+  (initPieces, lastPiece)
+    | null pieces
+    = ([], unsafeToPiece "index.html")
+    | Just (last pieces) == toPiece ""
+    = (init pieces, unsafeToPiece "index.html")
+    | otherwise
+    = let lastP = case fromPiece (last pieces) of
+            s | T.isSuffixOf ".txt" s -> last pieces
+            s                         -> unsafeToPiece $ s <> ".html"
+      in  (init pieces, lastP)
+
+-- | Convenience wrapper for @fileHelper@.
+fileHelperLR
+  :: ETagLookup
+  -> FilePath -- ^ file location
+  -> Piece -- ^ file name
+  -> IO LookupResult
+fileHelperLR a b c = fmap (maybe LRNotFound LRFile) $ fileHelper a b c
+
+-- | Attempt to load up a @File@ from the given path.
+-- This is taken and modified from WaiAppStatic.Storage.Filesystem.
+fileHelper
+  :: ETagLookup
+  -> FilePath -- ^ file location
+  -> Piece -- ^ file name
+  -> IO (Maybe File)
+fileHelper hashFunc fp name = do
+  efs <- try $ getFileStatus fp
+  case efs of
+    Left (_ :: SomeException)   -> return Nothing
+    Right fs | isRegularFile fs -> return $ Just File
+      { fileGetSize     = fromIntegral $ fileSize fs
+      , fileToResponse  = \s h -> Wai.responseFile s h fp Nothing
+      , fileName        = name
+      , fileGetHash     = hashFunc fp
+      , fileGetModified = Just $ modificationTime fs
+      }
+    Right _ -> return Nothing
+
+hashFileIfExists :: ETagLookup
+hashFileIfExists fp = do
+  res <- try $ hashFile fp
+  return $ case res of
+    Left  (_ :: SomeException) -> Nothing
+    Right x                    -> Just x
+
+-- | MD5 hash and base64-encode the file contents. Does not check if the file
+-- exists.
+hashFile :: FilePath -> IO ByteString
+hashFile fp = withBinaryFile fp ReadMode $ \h -> do
+  f <- BL.hGetContents h
+  let !hash = hashlazy f :: Digest MD5
+  return $ convertToBase Base64 hash
