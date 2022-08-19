fa0101cb9ecc305bd75c010374c45ef97ca20c13
Vo Minh Thu <thu@hypered.io>
2022-08-19 21:09:04 +0200

    Serve the static assets with Nginx.
    This avoids touching the backend, and will allow to try things such as
    Brotli compression.

--- modules/nginx.nix
+++ modules/nginx.nix
@@ -9,6 +9,9 @@
           proxyPass = "http://127.0.0.1:9100";
           extraConfig = "ssi on;";
         };
+        "/static/" = {
+          alias = (import ../.).static + "/";
+        };
         # TODO How to avoid hard-coding this 0.1.0.0 path ?
         "/haddock/".alias = (import ../.).haddock + "/share/doc/curiosity-0.1.0.0/html/";
       };
