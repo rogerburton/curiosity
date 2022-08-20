583068df910153a7d2790b04ab58399e0ab4e52d
Vo Minh Thu <thu@hypered.io>
2022-08-20 14:26:56 +0200

    Add Brotli compression to Nginx.
    This is done following the blog post
    https://dee.underscore.world/blog/brotlifying-nginx/
    
    - Use mainline Nginx
    - Add Brotly extension
    - Add a Nix function to apply Brotli compression
    - Use it on the static assets, that were already served by Nginx

--- modules/nginx.nix
+++ modules/nginx.nix
@@ -1,7 +1,20 @@
 { config, lib, pkgs, ... }:
+let
+  brotlify = pkgs.callPackage ./brotlify.nix { };
+  static = (import ../.).static;
+  static-layered = pkgs.buildEnv {
+    name = "static-layered";
+    paths = [
+      static
+      (brotlify { src = static; })
+    ];
+  };
+in
 {
   services.nginx = {
     enable = true;
+    package = pkgs.nginxMainline;
+    additionalModules = [ pkgs.nginxModules.brotli ];
     recommendedGzipSettings = true;
     virtualHosts."smartcoop.sh" = {
       locations = {
@@ -11,11 +24,15 @@
           extraConfig = "ssi on;";
         };
         "/static/" = {
-          alias = (import ../.).static + "/";
+          alias = static-layered + "/";
         };
         # TODO How to avoid hard-coding this 0.1.0.0 path ?
         "/haddock/".alias = (import ../.).haddock + "/share/doc/curiosity-0.1.0.0/html/";
       };
+
+      extraConfig = ''
+        brotli_static on;
+      '';
     };
   };
 }
