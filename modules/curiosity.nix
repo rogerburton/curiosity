e2a8b155d1526270b3cfd2dbbfdf85f74cd74219
Vo Minh Thu <thu@hypered.io>
2022-09-08 15:10:17 +0200

    Initialize the VM state when running the server.

--- modules/curiosity.nix
+++ modules/curiosity.nix
@@ -3,9 +3,20 @@
   systemd.services.curiosity = {
     wantedBy = [ "multi-user.target" ];
     script = ''
+      # Run from a state file, reset to a known state.
       # TODO system is meaningless for now.
+      rm -f /tmp/state.json
       ${(import ../.).binaries}/bin/cty \
-        --memory \
+        --state /tmp/state.json \
+        --user system \
+        init
+      ${(import ../.).binaries}/bin/cty \
+        --state /tmp/state.json \
+        --user system \
+        run ${(import ../.).scenarios}/state-0.txt
+
+      ${(import ../.).binaries}/bin/cty \
+        --state /tmp/state.json \
         --user system \
         serve \
         --server-port 9100 \
