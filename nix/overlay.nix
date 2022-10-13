43bbdb5e5156b37784fbe90dd618ee1e7341b3bd
Félix Baylac-Jacqué <felix@alternativebit.fr>
2022-10-13 13:34:11 +0200

    [nix] Filtering sources using a whitelist approach
    The Haskell build is quite expensive, we aggressively filter its src
    input to make sure we that editor's artifacts and other mutable state
    files don't end up triggering a rebuild.
    
    We take advantage of this filtering rework to move back the haskell
    sources filtering to the edge: we clean the derivation src before
    sending it to cabal2nix projects list. Doing this allow us to apply a
    package-specific filter instead of a generic one.

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -4,33 +4,13 @@ let
   lib = super.lib;
   sources = import ./sources.nix;
   contents = import ./contents.nix { nixpkgs = super; };
-
   inherit (super.lib.attrsets) mapAttrs;
   inherit (import sources.gitignore { inherit lib; }) gitignoreFilter;
 
   ourOverrides = selfh: superh:
     let
-
-      customFilter = src:
-        let
-          # IMPORTANT: use the following let binding to memoize info about the
-          # Git directories.
-          # (The comment is from the original example at
-          # https://github.com/hercules-ci/gitignore.nix/blob/master/docs/gitignoreFilter.md)
-          gitIgnoredSrc = gitignoreFilter src;
-        in
-          path: type: gitIgnoredSrc path type;
-
-      cleanedSrc = src: lib.sources.sourceFilesBySuffices
-        ( lib.cleanSourceWith {
-            filter = customFilter src;
-            src = src;
-            name = "source";
-          })
-        [".cabal" ".hs" ".json"]; # Keep JSON files for the test suite.
-
       callCabalOn = name: dir:
-        selfh.callCabal2nix "${name}" (cleanedSrc dir) { };
+        selfh.callCabal2nix "${name}" dir { };
 
     in mapAttrs callCabalOn contents.pkgList;
 
