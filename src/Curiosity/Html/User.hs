fb9261ccf998557c11ec0e2c189f3c524f1381a6
Vo Minh Thu <thu@hypered.io>
2022-11-09 10:23:09 +0100

    Remove code.
    That code belongs to the smart-design-hs library.

--- src/Curiosity/Html/User.hs
+++ src/Curiosity/Html/User.hs
@@ -72,98 +72,8 @@ instance H.ToMarkup ProfilePage where
         $ Just "Your email address is private."
       submitButton submitUrl "Update profile"
 
--- Partial re-creation of
--- https://design.smart.coop/prototypes/old-desk/contract-create-1.html
--- TODO Move to smart-design-hs and refactor.
-contractCreate1 =
-  containerMedium
-    $ H.div
-    ! A.class_ "o-form-group-layout o-form-group-layout--horizontal"
-    $ do
-        H.div ! A.class_ "o-form-group" $ do
-          H.label ! A.class_ "o-form-group__label" ! A.for "project" $ "Project"
-          H.div
-            ! A.class_
-                "o-form-group__controls o-form-group__controls--full-width"
-            $ do
-                H.button
-                  ! A.class_ "c-select-custom"
-                  ! customAttribute "aria-haspopup"       "listbox"
-                  ! customAttribute "data-menu"           "projects"
-                  ! customAttribute "data-menu-samewidth" "true"
-                  ! customAttribute "aria-expanded"       "false"
-                  $ H.div
-                  ! A.class_ "c-select-custom__value"
-                  $ "Select project"
-                H.ul
-                  ! A.class_ "c-menu c-menu--select-custom"
-                  ! A.role "listbox"
-                  ! A.id "projects"
-                  $ do
-                      H.li
-                        ! A.class_ "c-menu__item"
-                        ! A.role "option"
-                        $ H.div
-                        ! A.class_ "c-menu__label"
-                        $ "Project 1"
-                      H.li
-                        ! A.class_ "c-menu__item"
-                        ! A.role "option"
-                        $ H.div
-                        ! A.class_ "c-menu__label"
-                        $ "Project 2"
-                      H.li
-                        ! A.class_ "c-menu__item"
-                        ! A.role "option"
-                        $ H.div
-                        ! A.class_ "c-menu__label"
-                        $ "Project 3"
-                      H.li ! A.class_ "c-menu__divider" $ ""
-                      H.li
-                        ! A.class_ "c-menu__item"
-                        $ H.div
-                        ! A.class_ "c-menu__label"
-                        $ do
-                            H.div
-                              ! A.class_ "o-svg-icon o-svg-icon-add  "
-                              $ H.toMarkup svgIconAdd
-                            H.span "Add new project"
-                H.p
-                  ! A.class_ "c-form-help-text"
-                  $ "Enter an existing project or create new one"
-        H.div ! A.class_ "o-form-group" $ do
-          H.label
-            ! A.class_ "o-form-group__label"
-            ! A.for "description"
-            $ "Description"
-          H.div
-            ! A.class_
-                "o-form-group__controls o-form-group__controls--full-width"
-            $ do
-                H.textarea
-                  ! A.class_ "c-textarea"
-                  ! A.rows "5"
-                  ! A.id "description"
-                  $ ""
-                H.p
-                  ! A.class_ "c-form-help-text"
-                  $ "Describe your work (minimum 10 characters)"
-        H.div
-          ! A.class_ "o-form-group"
-          $ H.div
-          ! A.class_ "u-spacer-left-auto"
-          $ H.button
-          ! A.class_ "c-button c-button--primary"
-          ! A.type_ "button"
-          $ H.span
-          ! A.class_ "c-button__content"
-          $ do
-              H.span ! A.class_ "c-button__label" $ "Next"
-              H.div
-                ! A.class_ "o-svg-icon o-svg-icon-arrow-right  "
-                $ H.toMarkup svgIconArrowRight
 
-
+--------------------------------------------------------------------------------
 data ProfileView = ProfileView
   { _profileViewUserProfile   :: User.UserProfile
   , _profileViewEntitiesAndRoles :: [Legal.EntityAndRole]
@@ -264,6 +174,8 @@ displayEntitie (Legal.EntityAndRole entity role) =
       H.text . Legal.unRegistrationName $ Legal._entityName entity
     H.code . H.text $ show role
 
+
+--------------------------------------------------------------------------------
 data PublicProfileView = PublicProfileView
   { _publicProfileViewUserProfile   :: (Maybe User.UserProfile)
     -- ^ The logged in user, if any
@@ -305,30 +217,3 @@ publicProfileView profile =
                 (keyValuePair "Display name")
                 (User._userProfileDisplayName profile)
           maybe mempty (keyValuePair "Bio") (User._userProfileBio profile)
-
--- TODO Move to smart-design-hs and refactor.
-contractCreate1Confirm =
-  containerMedium $ H.div ! A.class_ "u-spacer-bottom-xl" $ do
-    H.div
-      ! A.class_ "u-spacer-bottom-l"
-      $ H.div
-      ! A.class_ "c-navbar c-navbar--unpadded c-navbar--bordered-bottom"
-      $ H.div
-      ! A.class_ "c-toolbar"
-      $ do
-          H.div
-            ! A.class_ "c-toolbar__left"
-            $ H.h3
-            ! A.class_ "c-h3 u-m-b-0"
-            $ "General information"
-          editButton "#"
-    H.dl
-      ! A.class_ "c-key-value c-key-value--horizontal c-key-value--short"
-      $ do
-          keyValuePair @Text "Worker" "Manfred"
-          keyValuePair @Text
-            "Work setting"
-            "The work is mainly performed in places and at times freely chosen"
-          keyValuePair @Text "Compensation budget" "1000.00 EUR"
-          keyValuePair @Text "Project" "Unspecified"
-          keyValuePair @Text "Description" "Some description."
